bfa78b3c18692183bdcd34186242883cd2d8bc3d
SofireOfficial <soph.johns247@gmail.com>
2021-12-02 22:09:04 +0000

    update

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function displayModal() {
   const modalContent = `<ol>
       <h2>Today, we shall...</h2>
       <ul>
-      <li>Move both alert buttons on to same line</li>
+      <li>Link socials to LearnReact page</li>
       <li style="background-color: rgba(216, 216, 23, 0.774)">Review LearnReact progress/functionality... <a href="https://sophlearnsreact.netlify.app/" target="blank">Click Here</a></li>
       </ul>
       <br>
